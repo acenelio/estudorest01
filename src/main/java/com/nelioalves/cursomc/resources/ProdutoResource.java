444e6a63fc6e6ee51ff1a120a3de5a9828b0ea8e
Nelio Alves <acenelio@gmail.com>
2017-09-03 18:17:28 -0300

    Dando tratamento correto aos casos de sucesso

--- src/main/java/com/nelioalves/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/ProdutoResource.java
@@ -1,13 +1,16 @@
 package com.nelioalves.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.cursomc.domain.Produto;
 import com.nelioalves.cursomc.services.ProdutoService;
@@ -20,28 +23,33 @@ public class ProdutoResource {
 	private ProdutoService produtoServico;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public List<Produto> list() {
-		return produtoServico.list();
+	public ResponseEntity<List<Produto>> list() {
+		return ResponseEntity.ok().body(produtoServico.list());
 	}
 
 	@RequestMapping(method=RequestMethod.POST)
-	public Produto insert(@RequestBody Produto obj) {
-		return produtoServico.insert(obj);
+	public ResponseEntity<Void> insert(@RequestBody Produto obj) {
+		obj = produtoServico.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public Produto find(@PathVariable Integer id) {
-		return produtoServico.find(id);
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		return ResponseEntity.ok().body(produtoServico.find(id));
 	}
 
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-	public void delete(@PathVariable Integer id) {
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		produtoServico.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public Produto update(@RequestBody Produto obj, @PathVariable Integer id) {
+	public ResponseEntity<Void> update(@RequestBody Produto obj, @PathVariable Integer id) {
 		obj.setId(id);
-		return produtoServico.update(obj);
+		obj = produtoServico.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 }
