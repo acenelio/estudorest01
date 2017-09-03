f9528a6bbf74bbb2c6eaf31c1e7f5ade09632a91
Nelio Alves <acenelio@gmail.com>
2017-09-03 12:08:53 -0300

    Testando a primeira classe de dominio

--- src/main/java/com/nelioalves/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/ProdutoResource.java
@@ -1,15 +1,27 @@
 package com.nelioalves.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Produto;
 
 @RestController
 @RequestMapping(value = "/produtos")
 public class ProdutoResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Produto> listar() {
+		Produto p1 = new Produto(1, "Mouse", 80.00);
+		Produto p2 = new Produto(2, "Teclado", 120.00);
+		
+		List<Produto> lista = new ArrayList<>();
+		lista.add(p1);
+		lista.add(p2);
+		
+		return lista;
 	}
 }
